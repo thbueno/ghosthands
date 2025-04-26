631e1fae2e9d52180dea70bac415c250db09fb95
thbueno <thiagobueno.s@gmail.com>
2025-04-26 12:35:34 +0700

    update colors AboutPage component for improved  readability and consistency

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,14 +11,14 @@ export default function AboutPage() {
   }, [])
 
   return (
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen bg-background">
       <div className="container mx-auto px-4 md:px-8 lg:px-24 py-12">
         {/* About Header */}
         <div className="mb-16 text-center md:text-left">
-          <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold leading-tight mb-8">
-            <span className="text-gray-400">About me,</span> a Visual Designer living in Munich
+          <h1 className="mb-8">
+            <span className="text-secondary">About me,</span> a Visual Designer living in Munich
           </h1>
-          <p className="text-lg md:text-xl text-gray-700 max-w-4xl mx-auto md:mx-0">
+          <p className="text-lg md:text-xl max-w-4xl mx-auto md:mx-0">
             As a Senior Designer with over 10 years of experience, I specialize in creating intuitive and user-centered
             interfaces for a wide range of digital products and experiences.
           </p>
@@ -44,13 +44,13 @@ export default function AboutPage() {
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
             <div>
-              <p className="text-gray-700">
+              <p>
                 With a collaborative mindset and a dedication to their craft, I&apos;m works closely with clients to
                 understand their goals and objectives, providing tailored design solutions that meet their unique needs.
               </p>
             </div>
             <div>
-              <p className="text-gray-700">
+              <p>
                 Outside of work, you can find me exploring the latest design trends, attending design conferences, or
                 working on personal projects.
               </p>
