85ce65987b022a99727d98cc662ac2441ec5d7be
thbueno <thiagobueno.s@gmail.com>
2025-05-05 00:31:02 +0700

    feat: add variable font settings for GeneralSans

--- app/fonts/index.ts
+++ app/fonts/index.ts
@@ -3,4 +3,6 @@ import localFont from 'next/font/local'
 export const myLocalFont = localFont({
   src: '../../public/fonts/GeneralSans.ttf',
   display: 'swap',
+  // Add variable font settings
+  variable: '--font-general-sans', // CSS variable name
 })
