4a4e62cf7a2d90867660f371813c346618aa7ee0
thbueno <thiagobueno.s@gmail.com>
2025-05-12 11:00:00 +0700

    Improve Work experience section

--- components/awards-section.tsx
+++ components/awards-section.tsx
@@ -16,7 +16,7 @@ const awards: Award[] = [
     id: "esthalo",
     title: "Esthalo - Advertising",
     year: "2022 to 2025",
-    organization: "Multimedia Software Engineer",
+    organization: "Digital Advertising Software Engineer",
     url: "#",
   },
   {
