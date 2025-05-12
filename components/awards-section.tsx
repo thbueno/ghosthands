8fcf85bcee4ee79599c82b8654b9870eceb5b213
thbueno <thiagobueno.s@gmail.com>
2025-05-12 11:00:00 +0700

    Job description

--- components/awards-section.tsx
+++ components/awards-section.tsx
@@ -16,7 +16,7 @@ const awards: Award[] = [
     id: "esthalo",
     title: "Esthalo - Advertising",
     year: "2022 to 2025",
-    organization: "Senior Software Engineeeer",
+    organization: "Senior Software Engineeeeer",
     url: "#",
   },
   {
