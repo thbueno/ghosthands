9c9d45583cad35079a7168c29b43b8f644130c06
thbueno <thiagobueno.s@gmail.com>
2025-09-07 23:54:20 +0700

    align profile image center

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -20,13 +20,13 @@ export function NavBar() {
       <header className="sticky top-0 left-0 right-0 bg-background z-50">
         <div className="container mx-auto px-4 md:px-8 lg:px-24 py-8">
           <div className="flex justify-between items-center">
-            <Link href="/" className="flex gap-2 text-title text-2xl font-bold">
+            <Link href="/" className="flex items-center align-middle text-title text-2xl font-bold">
             <Image
               src="/images/profile_photo.jpeg"
               alt="Designer portrait"
               width={180}
               height={180}
-              className="w-3 h-auto object-contain"
+              className="w-14 h-auto object-contain rounded-xl"
             />
               <span className="text-2xl font-bold">Thiago</span>Bueno
             </Link>
