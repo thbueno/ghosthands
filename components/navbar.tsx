fd122e9d9f879be20026c2f39723a9134a219463
thbueno <thiagobueno.s@gmail.com>
2025-09-07 23:37:39 +0700

    Navbar profile photo

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,6 +5,7 @@ import { LetsTalkButton } from "@/components/lets-talk-button";
 import { MenuButton } from "@/components/menu-button";
 import Link from "next/link";
 import { useState } from "react";
+import Image from "next/image";
 import { twMerge } from "tailwind-merge";
 
 export function NavBar() {
@@ -20,6 +21,13 @@ export function NavBar() {
         <div className="container mx-auto px-4 md:px-8 lg:px-24 py-8">
           <div className="flex justify-between items-center">
             <Link href="/" className="text-title text-2xl font-bold">
+            <Image
+              src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Showcasy_about_section.jpg-bUqRlOdy9atvjR6mivxT9jhrJIV3vU.jpeg"
+              alt="Designer portrait"
+              width={100}
+              height={600}
+              className="w-full h-auto object-cover"
+            />
               <span className="text-2xl font-bold">Thiago</span>Bueno
             </Link>
             <div className="flex items-center gap-4">
