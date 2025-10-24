e4bf705b10dd5314883e08f2d7f3b1abdfdf5f23
thbueno <thiagobueno.s@gmail.com>
2025-10-24 19:45:34 +0700

    tech tag component struture creation

---
+++ components/tech-tag.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+// Define the tags type
+interface TagProps {
+  name: string
+  logo: string
+}
+
+// frontend tags
+const frontend: TagProps[] = [
+  {
+    name: 'React',
+    logo: '/images/stack/react-logo.svg',
+  },
+  {
+    name: 'Next.js',
+    logo: '/images/stack/nextjs-logo.svg',
+  },
+  {
+    name: 'React Native',
+    logo: '/images/stack/vite-logo.svg',
+  },
+  {
+    name: 'Vite',
+    logo: '/images/django-logo.svg',
+  },
+  {
+    name: 'JavaScript',
+    logo: '/images/portal-logo.svg',
+  },
+  {
+    name: 'TypeScript',
+    logo: '/images/portal-logo.svg',
+  },
+  {
+    name: 'TypeScript',
+    logo: '/images/portal-logo.svg',
+  },
+]
+
+const backend: TagProps[] = [
+  {
+    name: 'React',
+    logo: '/images/stack/react-logo.svg',
+  },
+  {
+    name: 'Next.js',
+    logo: '/images/stack/nextjs-logo.svg',
+  },
+  {
+    name: 'Vite',
+    logo: '/images/stack/vite-logo.svg',
+  },
+  {
+    name: 'Django',
+    logo: '/images/django-logo.svg',
+  },
+  {
+    name: 'Portal',
+    logo: '/images/portal-logo.svg',
+  },
+]
+
+export default function TechTag() {
+  return <div>tech-tag</div>
+}
