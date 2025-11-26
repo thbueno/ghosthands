d97320600b587f98a6dea0b7941c7815eb242e4d
thbueno <thiagobueno.s@gmail.com>
2025-11-26 18:36:41 +0700

    skills section implementing a Grid system

--- components/skills-section.tsx
+++ components/skills-section.tsx
@@ -3,37 +3,41 @@ import TechTag, { FRONTEND_TAGS, BACKEND_TAGS } from '@/components/tech-tag'
 
 export default function SkillsSection() {
   return (
-    <section id="skills" className="px-7 py-20 md:px-8 lg:px-12">
-      <div className="conteiner flex flex-col gap-12 lg:flex-row">
-        <div className="flex flex-col gap-4">
-          <h2>Skills</h2>
-          <p className="text-gray-500">
-            These are the tools I’ve picked up along my journey as a developer,
-            not just through courses or tutorials, but by applying them in real,
-            production level projects.
-          </p>
-        </div>
-
-        {/* Frontend Section */}
-        <div className="flex flex-col gap-12">
-          <div className="flex flex-col gap-8">
-            <h3>Frontend</h3>
-            <TechTag tags={FRONTEND_TAGS} />
-
-            {/* Divider */}
-            <div className="flex items-center">
-              <div className="h-[1px] w-full bg-stone-400"></div>
-            </div>
+    <section id="skills" className="py-24 md:py-32">
+      <div className="container mx-auto md:px-8 lg:px-12">
+        <div className="grid grid-cols-1 gap-12 md:grid-cols-3">
+          <div className="md:col-span-1">
+            <h2>Skills</h2>
+            <p className="mt-4 text-gray-500">
+              These are the tools I've picked up along my journey as a
+              developer, not just through courses or tutorials, but by applying
+              them in real, production level projects.
+            </p>
           </div>
 
-          {/* Backend Section */}
-          <div className="flex flex-col gap-8">
-            <h3>Backend</h3>
-            <TechTag tags={BACKEND_TAGS} />
+          <div className="md:col-span-2">
+            <div className="flex flex-col gap-12">
+              {/* Frontend Section */}
+              <div className="flex flex-col gap-8">
+                <h3>Frontend</h3>
+                <TechTag tags={FRONTEND_TAGS} />
 
-            {/* Divider */}
-            <div className="flex items-center">
-              <div className="h-[1px] w-full bg-stone-400"></div>
+                {/* Divider */}
+                <div className="flex items-center">
+                  <div className="h-[1px] w-full bg-stone-400"></div>
+                </div>
+              </div>
+
+              {/* Backend Section */}
+              <div className="flex flex-col gap-8">
+                <h3>Backend</h3>
+                <TechTag tags={BACKEND_TAGS} />
+
+                {/* Divider */}
+                <div className="flex items-center">
+                  <div className="h-[1px] w-full bg-stone-400"></div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
